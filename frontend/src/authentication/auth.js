7c65b8f227166bd4ebfdfae1812d865314a81a75
Tyler <tylerjbrown34@gmail.com>
2026-03-07 13:57:06 -0500

    Some additions I made on the Authorization Login. Greater code refinements and login page to come soon.

--- frontend/src/authentication/auth.js
+++ frontend/src/authentication/auth.js
@@ -8,21 +8,23 @@ export const Auth = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    const signIn = async() => {
-        try{
-        await createUserWithEmailAndPassword(auth, email, password)
-        }catch(err){
+    const signIn = async () => {
+        try {
+            await createUserWithEmailAndPassword(auth, email, password)
+        } catch (err) {
             console.error(err)
+        }
+        return (
+            <div>
+                <input
+                    name="Email..."
+                    onChange={(e) => setEmail(e.target.value)}/>
+                <input
+                    name="Password..."
+                    onChange={(e) => setPassword(e.target.value)}/>
+                <button onClick={signIn}> Login</button>
+            </div>
+        )
     }
-    return (
-        <div>
-            <input
-                name="Email..."
-                onChange={(e) => setEmail(e.target.value)}/>
-            <input
-                name="Password..."
-                onChange={(e) => setPassword(e.target.value)}/>
-            <button onClick={signIn}> Login </button>
-        </div>
-    )
+
 }
