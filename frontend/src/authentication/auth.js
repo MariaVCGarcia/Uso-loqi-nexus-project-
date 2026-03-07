4f18ecce7ea759c6eeaf9b39da64a914aae7cde4
Tyler <tylerjbrown34@gmail.com>
2026-03-07 00:42:30 -0500

    Some additions I made on the Authorization Login. Greater code refinements and login page to come soon.

---
+++ frontend/src/authentication/auth.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { auth } from "./firebase";
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+
+
+
+export const Auth = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const signIn = async() => {
+        await createUserWithEmailAndPassword(auth, email, password)
+    }
+    return (
+        <div>
+            <input
+                name="Email..."
+                onChange={(e) => setEmail(e.target.value)}/>
+            <input
+                name="Password..."
+                onChange={(e) => setPassword(e.target.value)}/>
+            <button onClick={signIn}> Login </button>
+        </div>
+    )
+}
---
+++ frontend/src/authentication/registration.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { auth } from "./firebase";
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+
+
+
+export const Auth = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const registration = async() => {
+        await createUserWithEmailAndPassword(auth, email, password)
+    };
+
+
+    return (
+        <div>
+            <input
+                name="Email..."
+                onChange={(e) => setEmail(e.target.value)}/>
+            <input
+                name="Password..."
+                onChange={(e) => setPassword(e.target.value)}/>
+            <button onClick={registration}> Register </button>
+        </div>
+    )
+}
