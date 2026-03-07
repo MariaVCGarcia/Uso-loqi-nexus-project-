da917860f077080f8243bef7f69b28fd5cbf51db
Tyler <tylerjbrown34@gmail.com>
2026-03-07 01:02:26 -0500

    Some additions I made on the Authorization Login. Greater code refinements and login page to come soon.

--- frontend/src/authentication/firebase.js
+++ frontend/src/authentication/firebase.js
@@ -4,12 +4,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyADc4uV2eWcVfA5-_xM9yIwFvUDi5RBZ1U",
-    authDomain: "uso-loqi.firebaseapp.com",
-    projectId: "uso-loqi",
-    storageBucket: "uso-loqi.firebasestorage.app",
-    messagingSenderId: "113175929628",
-    appId: "1:113175929628:web:c514d998be266ece80d0d9"
+
 };
 
 // Initialize Firebase
